907bac966dc8843b0adb49196d7e8bc30ee1a576
Jagadeesh Kokkula <www.jagadeeshkokkula123@gmail.com>
2024-01-27 22:37:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+    var hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('active');
+  }
